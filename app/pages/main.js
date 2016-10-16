d180aba8a6fa49d3fa177fe749bbf4bb0774ffe3
Simon Brix <vdb.simon@gmail.com>
2016-10-16 19:31:36 +0200

    cleanup adding colors from app

--- app/pages/main.js
+++ app/pages/main.js
@@ -5,24 +5,8 @@ const navbar = require('../elements/navbar')
 const colorGroup = require('../elements/colorgroup')
 const colorTile = require('../elements/colortile')
 
-const globalStyle = sf`
-  .plus {
-    opacity: .5;
-    transition:opacity .15s ease-in;
-  }
-  .plus:hover,
-  .plus:focus {
-    opacity: .8;
-    transition:opacity .15s ease-in;
-  }
-  .plus:active {
-    opacity: 1;
-    transition:opacity .15s ease-out;
-  }
-`
-
 const main = (state, prev, send) => bel`
-  <div class="${globalStyle}" style="color: ${config.textColor}">
+  <div style="color: ${config.textColor}">
     ${navbar(config)}
     <main id="main" class="ph3 mt4 fixed absolute--fill overflow-scroll">
       ${config.colors.map((item) => {
